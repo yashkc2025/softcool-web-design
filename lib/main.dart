783491dd4001212f9bfd1804af4409e046e74309
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-09-09 17:26:15 +0530

    NavBar completed

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,67 @@ class _SoftCoolState extends State<SoftCool> {
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 50.0),
                       child: Container(
-                        height: 55,
-                        decoration: BoxDecoration(
-                            color: const Color(0xff141617),
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(15)),
-                            border: Border.all(color: Colors.white54)),
-                      ),
+                          height: 55,
+                          decoration: BoxDecoration(
+                              color: const Color(0xff141617),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(15)),
+                              border: Border.all(color: Colors.white54)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: const [
+                                  SizedBox(width: 30),
+                                  Text(
+                                    'SoftCool',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontFamily: 'Tabela',
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: const [
+                                  Text(
+                                    'About',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text(
+                                    'Services',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text('Projects',
+                                      style: TextStyle(color: Colors.white)),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text(
+                                    'Contact',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                width: 140,
+                                decoration: BoxDecoration(
+                                    color: Color(0xff242424),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(15)),
+                                    border: Border.all(color: Colors.white54)),
+                              ),
+                            ],
+                          )),
                     )
                   ],
                 ))));
