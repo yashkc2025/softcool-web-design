6505e96dc5195ced7ec4c20161093073f21b381e
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-09-09 18:15:55 +0530

    Extra containers removed

--- lib/main.dart
+++ lib/main.dart
@@ -19,152 +19,146 @@ class _SoftCoolState extends State<SoftCool> {
         home: SafeArea(
             child: Scaffold(
                 backgroundColor: Colors.black,
-                body: Container(
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 30),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                        child: Container(
-                            height: 55,
-                            decoration: BoxDecoration(
-                                color: const Color(0xff141617),
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(15)),
-                                border: Border.all(color: Colors.white54)),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: const [
-                                    SizedBox(width: 30),
-                                    Center(
-                                      child: Text(
-                                        'SoftCool',
-                                        // textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          fontFamily: 'Tabela',
-                                          color: Colors.white,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                        ),
+                body: Column(
+                  children: [
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                      child: Container(
+                          height: 55,
+                          decoration: BoxDecoration(
+                              color: const Color(0xff141617),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(15)),
+                              border: Border.all(color: Colors.white54)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: const [
+                                  SizedBox(width: 30),
+                                  Center(
+                                    child: Text(
+                                      'SoftCool',
+                                      // textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontFamily: 'Tabela',
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
                                       ),
                                     ),
-                                  ],
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: const [
+                                  Text(
+                                    'About',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text(
+                                    'Services',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text('Projects',
+                                      style: TextStyle(color: Colors.white)),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text(
+                                    'Contact',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                width: 140,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                    color: const Color(0xff242424),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(15)),
+                                    border: Border.all(color: Colors.white54)),
+                                child: const Center(
+                                  child: Text(
+                                    "Let's Talk",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w300,
+                                        fontFamily: 'Konstanz'),
+                                  ),
                                 ),
-                                Row(
-                                  children: const [
-                                    Text(
-                                      'About',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    SizedBox(
-                                      width: 20,
-                                    ),
-                                    Text(
-                                      'Services',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    SizedBox(
-                                      width: 20,
-                                    ),
-                                    Text('Projects',
-                                        style: TextStyle(color: Colors.white)),
-                                    SizedBox(
-                                      width: 20,
-                                    ),
-                                    Text(
-                                      'Contact',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ],
+                              ),
+                            ],
+                          )),
+                    ),
+                    const SizedBox(height: 60),
+                    Padding(
+                        padding: const EdgeInsets.only(left: 60, right: 100),
+                        child: Row(
+                          // mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Digital services to solve',
+                                  style: TextStyle(
+                                      fontSize: 50,
+                                      color: Colors.white,
+                                      fontFamily: 'Konstanz',
+                                      fontWeight: FontWeight.w500),
                                 ),
-                                Container(
-                                  width: 140,
-                                  height: 60,
-                                  decoration: BoxDecoration(
-                                      color: const Color(0xff242424),
-                                      borderRadius: const BorderRadius.all(
-                                          Radius.circular(15)),
-                                      border:
-                                          Border.all(color: Colors.white54)),
-                                  child: const Center(
-                                    child: Text(
-                                      "Let's Talk",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w300,
-                                          fontFamily: 'Konstanz'),
-                                    ),
+                                Text(
+                                  'business problems',
+                                  style: TextStyle(
+                                      fontSize: 50,
+                                      color: Colors.white,
+                                      fontFamily: 'Konstanz',
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                Text(
+                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum tellus massa,',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontFamily: 'Konstanz',
+                                  ),
+                                ),
+                                Text(
+                                  'nec tincidunt lorem faucibus vitae. Sed non ultricies arcu, ac tincidunt velit',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontFamily: 'Konstanz',
+                                  ),
+                                ),
+                                Text(
+                                  ' Morbi viverra faucibus nulla. Nullam varius ornare',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontFamily: 'Konstanz',
                                   ),
                                 ),
                               ],
-                            )),
-                      ),
-                      const SizedBox(height: 60),
-                      Padding(
-                          padding: const EdgeInsets.only(left: 60, right: 100),
-                          child: Container(
-                            // height: 100,
-                            child: Row(
-                              // mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: const [
-                                    Text(
-                                      'Digital services to solve',
-                                      style: TextStyle(
-                                          fontSize: 50,
-                                          color: Colors.white,
-                                          fontFamily: 'Konstanz',
-                                          fontWeight: FontWeight.w500),
-                                    ),
-                                    Text(
-                                      'business problems',
-                                      style: TextStyle(
-                                          fontSize: 50,
-                                          color: Colors.white,
-                                          fontFamily: 'Konstanz',
-                                          fontWeight: FontWeight.w500),
-                                    ),
-                                    SizedBox(
-                                      height: 30,
-                                    ),
-                                    Text(
-                                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum tellus massa,',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontFamily: 'Konstanz',
-                                      ),
-                                    ),
-                                    Text(
-                                      'nec tincidunt lorem faucibus vitae. Sed non ultricies arcu, ac tincidunt velit',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontFamily: 'Konstanz',
-                                      ),
-                                    ),
-                                    Text(
-                                      ' Morbi viverra faucibus nulla. Nullam varius ornare',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontFamily: 'Konstanz',
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                          )),
-                      SizedBox(
-                        height: 40,
-                      ),
-                    ],
-                  ),
+                            )
+                          ],
+                        )),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                  ],
                 ))));
   }
 }
